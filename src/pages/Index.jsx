fd1eef38216e6dc4822541da312948b452921e65
Talm333 <cristalmr12@gmail.com>
2025-07-04 01:32:22 -0400

    renderizacion

---
+++ src/pages/Index.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import PetManager from '../components/PetManager';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50">
+      <PetManager />
+    </div>
+  );
+};
+
+export default Index;
+
